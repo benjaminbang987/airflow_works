4c9c95aa8ee26debe0e63b32e458c45c0349b932
Benjamin Bang <benjamin.bang@cloverhealth.com>
2019-04-07 18:36:19 -0700

    add init files and edit dependencies;

--- setup/airflow_setup.py
+++ setup/airflow_setup.py
@@ -12,7 +12,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from airflow import models as af_models
-
+import os
 from tasks import game_1 as g1
 
 
@@ -37,8 +37,26 @@ t1 = BashOperator(
     dag=dag_game_1)
 
 t4 = PythonOperator(
+    task_id='run_game_1_and_store_results',
     python_callable=g1.game_1_main,
     op_kwargs={"database_url": "airflow_works",
                "n_players": 3,
-               "n_rounds": 100},
+               "n_games": 100},
     dag=dag_game_1)
+
+
+def symbolic_link(src_path, trg_path):
+    if not os.path.exists(src_path):
+        raise ValueError('Source path {src_path} does not exist'.format(src_path=src_path))
+    if not os.path.exists(trg_path):
+        raise ValueError('Target path {trg_path} does not exist'.format(trg_path=trg_path))
+    try:
+        os.symlink(src=src_path, dst=trg_path)
+    except FileExistsError:
+        print("DAG in {dag} is already connected to {trg_path}".format(dag=src_path,
+                                                                       trg_path=trg_path))
+
+
+if __name__ == "__main__":
+    symbolic_link("/Users/benjaminbang/dev/ben_prv/airflow_works/setup/airflow_setup.py", "/Users/benjaminbang/airflow/dags")
+    symbolic_link("/Users/benjaminbang/dev/ben_prv/airflow_works/setup/tasks/", "/Users/benjaminbang/airflow/dags")
